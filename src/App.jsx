506444b5c092413e8c628ce0967fad3c51b2629a
Lylyneru <lyly777@gmail.com>
2025-02-28 11:26:43 +0200

    fix main. app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
 import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
@@ -8,16 +5,12 @@ import s from "./App.module.css";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div className={s.container}>
-          <h1>Phonebook</h1>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-        </div>
-      </PersistGate>
-    </Provider>
+    <div className={s.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 };
 
